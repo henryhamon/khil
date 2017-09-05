f4a1c2fd86c4dbf2aa8e730d421590c57018f78f
HammZ <henryhamon@gmail.com>
2017-09-05 20:04:36 +0000

    validate an unic room name

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe Room, type: :model do
   # Validation tests
   # ensure columns title and created_by are present before saving
   it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name)}
 end
