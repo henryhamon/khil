c385c0e177f00124569c778ec1e80211323dda7c
HammZ <henryhamon@gmail.com>
2017-09-06 17:10:49 +0000

    first route

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,45 @@
+ #app/controllers/rooms_controller.rb
 class RoomsController < ApplicationController
+
+  before_action :set_room, only: [:show, :update, :destroy]
+
+  # GET /rooms
+  def index
+    @rooms = room.all
+    json_response(@rooms)
+  end
+
+  # POST /rooms
+  def create
+    @room = Room.create!(room_params)
+    json_response(@room, :created)
+  end
+
+  # GET /rooms/:id
+  def show
+    json_response(@room)
+  end
+
+  # PUT /rooms/:id
+  def update
+    @room.update(room_params)
+    head :no_content
+  end
+
+  # DELETE /rooms/:id
+  def destroy
+    @room.destroy
+    head :no_content
+  end
+
+  private
+
+  def room_params
+    # whitelist params
+    params.permit(:name, :color)
+  end
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
 end
